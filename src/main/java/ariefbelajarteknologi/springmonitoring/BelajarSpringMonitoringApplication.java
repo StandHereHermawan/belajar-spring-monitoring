e19409fe1ab19422c61b3f55e012882859ef5c3b
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-26 21:28:34 +0700

    Setup Project, Initial commit

---
+++ src/main/java/ariefbelajarteknologi/springmonitoring/BelajarSpringMonitoringApplication.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.springmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ariefbelajarteknologi/springmonitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.springmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
